68fdec0673ee4571cac35be2f7ffea5e9e770d53
Andrew Perrin <andrew.perrin@cgi.com>
2019-09-18 17:26:20 -0400

    initial commit

---
+++ src/main/java/com/perrin/helloservice/HelloServiceApplication.java
@@ -0,0 +1,13 @@
+package com.perrin.helloservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloServiceApplication.class, args);
+	}
+
+}
